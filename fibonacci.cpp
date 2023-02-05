0bfd0d8dfd60a51a0f75c111bda8903bbbcc5641
tamannachowdhuryy <104877190+tamannachowdhuryy@users.noreply.github.com>
2023-02-04 20:21:08 -0500

    Update fibonacci.cpp

--- fibonacci.cpp
+++ fibonacci.cpp
@@ -6,6 +6,7 @@ Instructor: Zamankey
 Assignment: Computing Fibonacci Numbers with Loops and Arrays 0, 1, 1, 2, 3, 5,
 8, 13…. Lab2D
 */
+  
 #include <iostream>
 int main() {
   // this is the start of the array where we decleared that the array will end
@@ -21,7 +22,7 @@ int main() {
   }
   for (int i = 0; i < 60; i++) {
     // this will print everything out in a list
-    std::cout << fib[i] << std::endl;
+    std::cout << fib[i] << "\n";
   }
   return 0;
 }
